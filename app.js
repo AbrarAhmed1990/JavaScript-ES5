d28933cf5b9bdf3c0afba97eb97fdb6d402986d6
Abrar Ahmed <appdev1990w@gmail.com>
2024-02-26 01:39:59 -0800

    added Table to JS file

---
+++ app.js
@@ -0,0 +1,133 @@
+// <-------- CHAPTER NO 4---------->
+
+// Q NO: 1
+
+// var a = "John" , b = "Doe" , age = 21;
+
+// document.write("My name is "+a+" "+b+" and my age is"+age+" year old")
+
+// Q NO: 2
+
+    // <--illegal Variables-->
+// var 2a = "abc" ; 
+// var @a = "abc" ;
+// var a-b = "abc" ;
+// var 1223 = "abs" ;
+// var a-b-c = "asfd" ;
+
+    // <--legal Variables-->
+// var a = "abc" ;
+// var a2 = "abc" ;
+// var $abc = "abc" ;
+// var a_bc = "abc" ;
+// var firstName = "abc" ;
+
+// Q NO : 3
+
+// document.write("<h1>Rules for naming JS varibles</h1>") ;
+
+// var $my_1stVarible = "abc" , $my_2ndVariable = "123"
+
+// document.write ($my_1stVarible+$my_2ndVariable);
+
+// var John = "abc" , $John = "abcd" , _John = "abcde" ;
+
+// document.write(John+" "+$John+" "+_John)
+
+// <-------- CHAPTER NO 4 END---------->
+
+
+// <-------- CHAPTER NO 5---------->
+
+// Q NO : 1
+
+// var x = 3 , y = 5 ;
+
+// var a =(x+y) , b = (x-y) , c = (x*y) ;
+
+// document.write("<h1>"+"Sum of "+x+" and "+y+" is "+a+"</h1>"
+// +"<br>"+"<h1>"+"Subtraction of "+x+" and "+y+" is "+b+"</h1>"+
+// "<br>"+"<h1>"+"Multipliction of "+x+" and "+y+" is "+c+"</h1>"+"<br>");
+
+// Q NO : 2
+
+// var x = 35 , y = 85 ;
+
+// var b = (x-y) , c = (x*y) , e = (y/x) ;
+
+// document.write("<h1>"+"Subtraction of "+x+" and "+y+" is "+b
+// +"</h1>"+"<br>"+"<h1>"+"Multipliction of "+x+" and "+y+" is "+c
+// +"</h1>"+"<br>"+"<h1>"+"Divsion of "+y+" into "+x+" is "+e+"</h1>");
+
+// Q NO : 3
+
+// var a;
+
+// document.write("Value after variable declaration is "+a+"<br>") ;
+
+// var a = 5 ;
+
+// document.write("Initial value :"+a+"<br>") ;
+
+// var a = ++a ;
+
+// document.write("Value after increament is:"+a+"<br>") ;
+
+// var a = a + 7 ;
+
+// document.write("Value after addition is:"+a+"<br>") ;
+
+// var a = --a ;
+
+// document.write("Value after decreamnet is:"+a+"<br>") ;
+
+// var a = a / 3 ;
+
+// document.write("The remainder is:"+a+"<br>") ;
+
+// Q NO : 4
+
+// var a = 600 ;
+
+// document.write("Total cost to buy 5 tickets to movie is "+a*5+"PKR")
+
+// Q NO : 5
+
+// var a = prompt("Enter your table digit") ;
+
+// document.write
+// ("Table of "+a+"<br>"+a+" x 1 = "+a*1+"<br>"+
+// a+" x 2 = "+a*2+"<br>"+
+// a+" x 3 = "+a*3+"<br>"+
+// a+" x 4 = "+a*4+"<br>"+
+// a+" x 5 = "+a*5+"<br>"+
+// a+" x 6 = "+a*6+"<br>"+
+// a+" x 7 = "+a*7+"<br>"+
+// a+" x 8 = "+a*8+"<br>"+
+// a+" x 9 = "+a*9+"<br>"+
+// a+" x 10 = "+a*10+"<br>")
+
+// Q NO : 6
+
+// var C = 25 , F = 70 ;
+
+// document.write(C+"<sup>o</sup>C is ",(C*9/5)+32+"<sup>o</sup>"+"<br>"+F+"<sup>o</sup>F is ",(F-32)*5/9,"<sup>o</sup>C")
+
+// Q NO : 7
+
+// var item1 = 650 , item2 = 100 ;
+
+// document.write("<h1>Shopping Cart</h1>","<br>","Price of item 1 is ",
+// item1,"<br>","Quantity of item 1 is 3","<br>","Price of item 2 is ",item2,"<br>"
+// ,"Quantity of item 2 is 7","<br>","Shipping Charges 100","<br>","<br>",
+// "Total cost of your order is ",item1*3+item2*7+100) ;
+
+// Q NO : 8
+
+// var a = 980 , b = 804 ;
+
+// document.write("<h1>Marks Sheet</h1><br><br>","Total marks :",
+// a,"<br> Marks obtained :",b,"<br> Percentage :",b/a*100,"%")
+
+
+// <-------- CHAPTER NO 5 END---------->
